eb40f6b5129d1061f53c4fa45e6fc67c55999d5c
vikrantkamatpartner <vikrant.kamat@partner.bmwgroup.com>
2025-04-24 20:34:33 +0530

    JS added for song playing functionality

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
 console.log("Let's start JS")
+let currentSong = new Audio();
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text()
+    console.log(response)
+
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let ahref = div.getElementsByTagName("a")
+    console.log(ahref)
+
+    let songs = []
+
+    for (const a of ahref) {
+        if (a.href.endsWith(".mp3")) {
+            songs.push(a.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
+
+function playMusic(track){
+    //var audio = new Audio("/songs/" + track);
+    currentSong.src = "/songs/" + track;
+    currentSong.play();
+}
+
+async function main(params) {
+    // get the list of all songs
+    let songs = await getSongs()
+    console.log(songs)
+
+    let songList = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songList.innerHTML = songList.innerHTML + `<li>
+                        <img class="invert" src="play-circle-02-stroke-rounded.svg" alt="">
+                        <div class="songinfo">
+                            <div>${song.replaceAll("%20", " ")}</div>
+                            <div>A.R. Rahman</div>
+                        </div>
+                    </li>`
+    }
+
+    //playing first song
+    // var audio = new Audio(songs[3]);
+    // audio.play();
+
+    // audio.addEventListener("loadeddata", ()=>{
+    //     let duration = audio.duration;
+    //     console.log(duration)
+    // });
+
+    //attac an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click", elem=>{
+            console.log(e.getElementsByTagName("div")[0].firstElementChild.innerHTML);
+            playMusic(e.getElementsByTagName("div")[0].firstElementChild.innerHTML)
+        })
+    })
+}
 
 
+main()
